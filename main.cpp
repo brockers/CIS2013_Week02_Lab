29bc76a2d79bd71dc172eaa1291befcee36c76c6
Bob Rockers <rockers@okstate.edu>
2019-01-30 19:47:48 -0600

    Testing boolean operations and varaible types

--- main.cpp
+++ main.cpp
@@ -2,22 +2,28 @@
 using namespace std;
 int main(){
 	cout << "Week 2 Lab:" << endl;
-	int bob;
-	int john;
 	
-	bob = 42;
-	john = 32;
+	int bob = 42;
+	int john = 32;
 	int jimbob = bob + john;
 	short rachel = 10;
 	short luke = 7;
 	float pie = 3.14;
 	char middle = 'M';
+	bool shouldWe = false;
 	
-	cout << "Bob and John together are ";
-	cout << jimbob << " years old" << endl;
-	cout << middle << " is the middle initial for ";
-	cout << pie << " users that are " << luke;
-	cout << " year old.";
+	if('M' == middle){
+		cout << "The result was true" << endl;
+	} else {
+		cout << "FALSE!!!!" << endl;
+	}
+	
+	cout << jimbob << " is bob and jim added" << endl;
+	cout << bob - john << " is bob and jim subtracted" << endl;
+	cout << bob / john << " is bob and jim divided" << endl;
+	cout << bob * john << " is bob and jim multiplied" << endl;
+	cout << bob % john << " is bob modulas jim" << endl;
+	
 	
     return 0;
 }
