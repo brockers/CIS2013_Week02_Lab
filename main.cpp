ccb395b7a5a15eccc0faaf153903b7ed281bd081
Bob Rockers <rockers@okstate.edu>
2019-01-30 19:16:52 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "Week 2 Lab:" << endl;
+	int bob;
+	int john;
+	
+	bob = 42;
+	john = 32;
+	int jimbob = bob + john;
+	short rachel = 10;
+	short luke = 7;
+	float pie = 3.14;
+	char middle = 'M';
+	
+	cout << "Bob and John together are ";
+	cout << jimbob << " years old" << endl;
+	cout << middle << " is the middle initial for ";
+	cout << pie << " users that are " << luke;
+	cout << " year old.";
+	
+    return 0;
+}
